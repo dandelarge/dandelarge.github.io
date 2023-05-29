f69b327322253f7baf54cc0b91b02eff8320b569
dandelarge <sdarauz@gmail.com>
2023-05-29 17:07:26 +0200

    scafolding nextjs app

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (<>
+    <header>
+      <nav className="p-4 justify-between">
+        <h3>salsascript.dev</h3>
+      </nav>
+    </header>
+    <main className="flex min-h-screen flex-col items-center justify-center p-4">
+      <h1 className="text-8xl text-orange-400 ">Under construction</h1>
+    </main>
+  </>)
+}
