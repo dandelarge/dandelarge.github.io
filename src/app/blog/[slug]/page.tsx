0f4f4b37cba272db5e65d8def265693bddddf20a
Daniel Arauz <daniel.arauz@fuga.com>
2025-03-30 17:43:46 +0200

    added the static params for dynamic routes

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -2,9 +2,12 @@ import BlogSubMenu from "@/components/BlogSubmenu";
 import PreviewImg from "@/components/PreviewImg";
 import SideNavLayout from "@/components/layouts/sidenav-layout";
 import getPosts from "@/utils/getPosts";
-import { serialize } from "next-mdx-remote/serialize";
 import { MDXRemote } from "next-mdx-remote/rsc";
 
+export function generateStaticParams() {
+  const posts = getPosts();
+  return posts.map((post) => ({ slug: post.slug }));
+}
 export default async function BlogPostPage({
   params,
 }: {
@@ -13,7 +16,6 @@ export default async function BlogPostPage({
   const { slug } = await params;
   const posts = getPosts();
   const currentPost = posts.find((post) => post.slug === slug);
-  const compiled = await serialize(currentPost?.content || "");
 
   const article = await MDXRemote({
     source: currentPost?.content || "",
