b7d9bbb968c5265b4ae4a535a47219c5edd8e267
Daniel Arauz <daniel.arauz@fuga.com>
2025-04-06 23:45:12 +0200

    fixing the invisible close button

--- src/components/PreviewImg.tsx
+++ src/components/PreviewImg.tsx
@@ -24,12 +24,11 @@ function Modal({
         top: `${documentTop}px`,
       }}
     >
-      <button
-        className="fixed top-0 right-0 p-4 text-white text-lg"
-        onClick={onClose}
-      >
-        Close
-      </button>
+      <div className="w-full flex flex-col gap-12 justify-center items-end">
+        <button className="p-4 text-white text-lg" onClick={onClose}>
+          Close
+        </button>
+      </div>
 
       {children}
     </div>
